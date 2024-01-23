7beccbb246e446caf112321407b9a202a93c4799
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 22:23:53 +0600

    Add files via upload

---
+++ Python_if_else.py
@@ -0,0 +1,57 @@
+#exercise1
+a = 50
+b = 10
+if a>b:
+  print("Hello World")
+
+#exercise2
+a = 50
+b = 10
+if a!=b:
+ print("Hello World")
+
+#exercise3
+ a = 50
+b = 10
+if a==b:
+  print("Yes")
+else:
+  print("No")
+
+#exerice4
+  a = 50
+b = 10
+if a==b:
+ print("1")
+if a>b:
+  print("2")
+else:
+  print("3")
+
+#exercise5
+  if a == b and c == d:
+    print("Hello")
+
+
+#exercise6
+if a == b or c == d:
+  print("Hello")
+    
+
+#exercise7
+  if 5 > 2:
+   print("YES")
+
+#exercise8
+a = 2
+b = 5
+print("YES") if a==b else print("NO")
+
+
+#exercise9
+a = 2
+b = 50
+c = 2
+if a==c or b==c:
+  print("YES")
+
