b2d76c5881cd6ad60708c1f8901fc0d92b1c8bce
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 21:01:59 +0600

    Add files via upload

---
+++ Pyhton_tuples.py
@@ -0,0 +1,16 @@
+#exercise1
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+#exercise2
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+#exercise3
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+#exercise4
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+
