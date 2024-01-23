b04591a43439da5a77e0fe2086c85b18a527eda2
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 21:59:01 +0600

    Add files via upload

---
+++ Pethon_sets.py
@@ -0,0 +1,26 @@
+#exercise1
+fruits = {"apple", "banana", "cherry"}
+if "apple"in fruits:
+  print("Yes, apple is a fruit!")
+
+#exercise2
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+#exercise3
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+
+#exercise4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+
+#exercise5
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+
+
