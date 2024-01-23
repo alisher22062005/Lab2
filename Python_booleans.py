5922fe28daa71365e76099a1251e5af770d5bd84
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 19:59:40 +0600

    Add files via upload

---
+++ Python_booleans.py
@@ -0,0 +1,59 @@
+#example1
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+#example2
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+#example3
+def myFunction() :
+  return True
+
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+ #example4
+  x = 200
+print(isinstance(x, int)) 
+
+
+#exercise1
+print(10 > 9)# True
+
+
+#exercise2
+print(10 == 9) # False
+
+
+#exercise3
+print(10 < 9)# False
+
+
+#exercise4
+print(bool("abc"))# True
+
+
+#exercise5
+print(bool(0)) # False
+
+
+
+
+
+
