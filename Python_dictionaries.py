1490a354a727c837205752f54ef98cde1ea6fa30
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 22:08:23 +0600

    Add files via upload

---
+++ Python_dictionaries.py
@@ -0,0 +1,43 @@
+#exercise1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+#exercise2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+  }
+car["year"]=2020
+
+#exercise3
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"]="red"
+
+#exercise4
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+#exercise5
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+
+
+
+
