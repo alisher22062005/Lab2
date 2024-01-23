a4769c79a1fb95ceafc96509db2027b8f8373462
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 20:32:00 +0600

    Add files via upload

---
+++ Python_operators.py
@@ -0,0 +1,18 @@
+#exercise1
+print(10*5) 
+
+#exercise2
+print(10/2)
+
+#exercise3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#exercise4
+  if 5!=10:
+   print("5 and 10 is not equal")
+
+#exercise5
+   if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
