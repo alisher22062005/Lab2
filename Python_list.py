39947f5781f725a8939ccb99de3eb4eca972f75b
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-23 22:40:03 +0600

    Add files via upload

---
+++ Python_list.py
@@ -0,0 +1,33 @@
+#exercise1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#exercise2
+fruits = ["apple", "banana", "cherry"]
+fruits[0]= "kiwi"
+
+#exercise3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+
+#exercise4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+
+#exercise5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#exercise6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#exercise7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#exercise8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
